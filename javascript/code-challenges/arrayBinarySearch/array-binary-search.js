34b422b60c60d4ecf65cb69cc0d54bbf686367e7
Chris Gantt <gantt.art@gmail.com>
2021-04-21 11:43:02 -1000

    Implemented binary search, all tests passing, included whiteboard

--- javascript/code-challenges/arrayBinarySearch/array-binary-search.js
+++ javascript/code-challenges/arrayBinarySearch/array-binary-search.js
@@ -1,7 +1,37 @@
 'use strict';
 
-function binarySearch(array, value) {
-  console.log(array, value);
+function bsHelper(sortedArray, target, start, end) {
+  let middle = Math.floor((start + end) / 2);
+
+  if (target === sortedArray[start]){
+    return start;
+  }
+  else if (target === sortedArray[middle]) {
+    return middle;
+  }
+  else if (target === sortedArray[end]) {
+    return end;
+  }
+  else if ((middle === end) || (middle === start)) {
+    return -1;
+  }
+  else if (sortedArray[middle] < target) {
+    return bsHelper(sortedArray, target, middle, end);
+  }
+  else if (sortedArray[middle] > target) {
+    return bsHelper(sortedArray, target, start, middle);
+  }
+}
+
+function binarySearch(sortedArray, target) {
+  const arrayLength = sortedArray.length;
+  if ((arrayLength === 0) ||
+      (target < sortedArray[0]) ||
+      (target > sortedArray[arrayLength - 1])) {
+    return -1;
+  } else {
+    return bsHelper(sortedArray, target, 0, (arrayLength - 1));
+  }
 }
 
 module.exports = binarySearch;
