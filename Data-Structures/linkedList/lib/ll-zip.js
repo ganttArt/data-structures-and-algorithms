65f9ce58e250e6cceca2bc3c4c0aaca6f4609063
Chris Gantt <gantt.art@gmail.com>
2021-04-28 12:07:45 -1000

    Second attempt at algorithm, two more tests passing.

--- Data-Structures/linkedList/lib/ll-zip.js
+++ Data-Structures/linkedList/lib/ll-zip.js
@@ -8,7 +8,29 @@ function zipLists(list1, list2) {
   } else if (!list2.head) {
     return list1;
   }
-  return null;
+  console.log('precurrent');
+  let current1 = list1.head;
+  let current2 = list2.head;
+  while (current1) {
+    if (current2) {
+      // attempt 2
+      let list1temp = current1.next;
+      current1.next = current2;
+      current2 = current2.next;
+      current1.next.next = list1temp;
+      current1 = current1.next.next;
+
+      // // attempt 1
+      // let nextOne = current1.next;
+      // let temp = current2;
+      // current1.next = temp;
+      // temp.next = nextOne;
+      // current2 = current2.next;
+      // current1 = current1.next.next;
+    }
+    else { break; }
+  }
+  return list1;
 }
 
 module.exports = zipLists;
