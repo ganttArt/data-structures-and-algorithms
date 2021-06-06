c3096e9b3d4fe47951bf920472e0419137f1ecc2
Chris Gantt <gantt.art@gmail.com>
2021-06-06 06:56:59 -1000

    Implemented and tested hashtable implementation.

--- Data-Structures/hashtable/hashtable.js
+++ Data-Structures/hashtable/hashtable.js
@@ -10,6 +10,7 @@ class Node {
 class LL {
   constructor() {
     this.head = null;
+    this.length = 0;
   }
 
   add(val) {
@@ -21,6 +22,7 @@ class LL {
       node.next = this.head;
       this.head = node;
     }
+    this.length += 1;
   }
 
   getList() {
@@ -45,7 +47,11 @@ class Hashmap {
     this.size = size;
   }
 
-  hash(key) { // this hash function taks a string, splits it into an array, grabs the char code for each character in the string array, then multiples the accumulated set by a prime number, then grab the remainder and this is our index placement
+  hash(key) {
+    // this hash function takes a string, splits it into an array,
+    // grabs the char code for each character in the string array,
+    // then multiples the accumulated set by a prime number,
+    // then grab the remainder and this is our index placement
     return key.split('').reduce((acc, cur) => {
       return acc + cur.charCodeAt(0);
     }, 0) * 19 % this.size;
@@ -66,16 +72,52 @@ class Hashmap {
       this.storage[hash].add([key, val]);
     }
   }
+
+  get(key) {
+    let hash = this.hash(key);
+    let bucket = this.storage[hash];
+
+    if (!this.storage[hash] || !bucket.head) return null;
+
+    let current = bucket.head;
+
+    while(current) {
+      if (current.val[0] === key) return current.val[1];
+      current = current.next;
+    }
+    return null;
+  }
+
+  contains(key) {
+    let hash = this.hash(key);
+    let bucket = this.storage[hash];
+
+    if(!this.storage[hash] || !bucket.head) return false;
+
+    let current = bucket.head;
+
+    while(current) {
+      if (current.val[0] === key) return true;
+      current = current.next;
+    }
+    return false;
+  }
 }
 
-let hashmap = new Hashmap(3);
+// let hashmap = new Hashmap(3);
 
-hashmap.set('brian', 'nations');
-hashmap.set('alex', 'boberson');
-hashmap.set('alex', 'johnson');
-hashmap.set('alex', 'thompson');
-hashmap.set('xale', 'coolness');
-hashmap.set('sally', 'samsonite');
-hashmap.set('tom', 'tiller');
+// hashmap.set('brian', 'nations');
+// hashmap.set('alex', 'boberson');
+// hashmap.set('alex', 'johnson');
+// hashmap.set('alex', 'thompson');
+// hashmap.set('xale', 'coolness');
+// hashmap.set('sally', 'samsonite');
+// hashmap.set('tom', 'tiller');
 
-console.log(hashmap);
+// console.log(hashmap);
+
+module.exports = {
+  Hashmap: Hashmap,
+  Node: Node,
+  LL: LL
+};
